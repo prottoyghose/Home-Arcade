d8ca4db1d912c5a2deb01d9c993c5adf8f36729e
dillpickle5051 <dm965111@ohio.edu>
2016-07-22 04:03:45 -0400

    Add files via upload
    UI-V3

---
+++ faq.php
@@ -0,0 +1,73 @@
+<?php include("./header.php") ?>
+
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <h1 align = "center">Frequently Asked Questions</h1><hr>
+
+       <h4> <b>Q: Can I use this application even if I don't have insurance with Nationwide? </b></h4>
+        <div class = "panel panel-default"> 
+          <div class = "panel panel-body"> 
+            <h4> A: Yes! HomeArcade is open to Nationwide members as well as non-members.</h4> 
+          </div>
+        </div>
+
+        <h4> <b>Q: By storing my items on this website do I automatically have coverage with Nationwide? </b></h4>
+        <div class = "panel panel-default"> 
+          <div class = "panel panel-body"> 
+            <h4> A: No. To obtain insurance coverage with Nationwide, you can visit the <a href = "https://www.nationwide.com/" target = "_blank"> nationwide homepage</a> or call  1-877-On Your Side (1-877-669-6877).  </h4> 
+          </div>
+        </div>
+
+             <h4> <b>Q: How do I start my inventory on HomeArcade? </b></h4>
+            <div class = "panel panel-default"> 
+              <div class = "panel panel-body"> 
+                <h4> A: Start off by creating an account by clicking the sign up link. Once you have an account created or have logged in successfully you will see a "Add Item" button. You can add as many items as you want.   </h4> 
+              </div>
+            </div>
+
+         <h4> <b>Q: How many items can I store? </b></h4>
+          <div class = "panel panel-default"> 
+            <div class = "panel panel-body"> 
+              <h4> A: There is anuxx unlimited amount of items your can store. </h4> 
+            </div>
+          </div>
+
+         <h4> <b>Q: Can I add family member's assets to my home inventory? </b></h4>
+          <div class = "panel panel-default"> 
+            <div class = "panel panel-body"> 
+              <h4> A: Yes. HomeArcade offers the option to add other people onto your home inventory. However, if their assets reside at another location, a subcategory of their property will be listed under your account.  </h4> 
+            </div>
+          </div>
+
+         <h4> <b>Q: Can Nationwide access and view my items? </b></h4>
+          <div class = "panel panel-default"> 
+            <div class = "panel panel-body"> 
+              <h4> A: No. Only the user can access the items on the account. However, when you file a claim, you can choose the option of sharing this inventory with an agent to help aid in the claims proccess.   </h4> 
+            </div>
+          </div>
+
+          <h2> Any more questions? </h2>
+          <h4>We'll try our best to answer any questions you may have and respond as soon as possible.</h4>
+          <form role = "form">
+
+           <div class = "form-group">
+              <label input = "text"> Question: </label>
+              <input type = "text" class = "form-control" id = "text" placeholder="Enter question">
+            </div> 
+
+            <div class = "form-group">
+              <label for = "email"> Email: </label>
+              <input type = "email" class = "form-control" id = "email" placeholder="Enter email">
+            </div> 
+
+            <button type = "submit" class = "btn btn-default"> Submit Question </button>
+
+          </form>
+            
+            
+            
+
+        </div>           
+      </div>
+    </div>
+
+<?php include("./footer.php") ?>
